fe033a47362ac20510562c6496fd1fefb5939175
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-08 18:41:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// export default {
+//   base: '/Seats_Booking_ReactJS/',
+// };
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Seats_Booking_ReactJS/', // Set the base path for GitHub Pages
   plugins: [react()],
-})
+});
 
-export default {
-  base: '/Seats_Booking_ReactJS/',
-};
