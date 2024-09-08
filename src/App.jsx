4702bcd83b676ed2dacf62036077cff4d58e1cec
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-08 18:58:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Coach from "./Coach"
+import Coach from "./Coach";
 
 function App() {
   return (
-    <Coach/>
-    <a href="https://github.com/Sai-Chakradhar-Mahendrakar/Seats_Booking_ReactJS"><h1>Github Link</h1></a>
-  )
+    <div>
+      <Coach />
+      <a href="https://github.com/Sai-Chakradhar-Mahendrakar/Seats_Booking_ReactJS">
+        <h1>Github Link</h1>
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
