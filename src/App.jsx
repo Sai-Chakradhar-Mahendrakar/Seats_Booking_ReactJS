903243c2106938668a567fbe08a97c764338ea43
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-08 18:58:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Coach from "./Coach"
 
 function App() {
-  return <Coach/>
+  return (
+    <Coach/>
+    <a href="https://github.com/Sai-Chakradhar-Mahendrakar/Seats_Booking_ReactJS"><h1>Github Link</h1></a>
+  )
 }
 
 export default App
