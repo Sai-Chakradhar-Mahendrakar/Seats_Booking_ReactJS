26b5bb25d2b6e36905d8fee1787989e971ee86a8
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-08 18:57:31 +0530

    Update Coach.jsx

--- src/Coach.jsx
+++ src/Coach.jsx
@@ -106,7 +106,6 @@ const Coach = () => {
           );
         })}
       </div>
-      <a href="https://github.com/Sai-Chakradhar-Mahendrakar/Seats_Booking_ReactJS"><h1>Github Link</h1></a>
     </div>
   );
 };
